823adc95489370228e888ceab798220825526878
JSM788 <salinasjuan788@gmail.com>
2022-08-14 21:10:24 -0500

    Adding style

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,9 @@ include stdlib
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/sshd_config',
-  line   => '#   PasswordAuthentication no',  
+  line   => '#   PasswordAuthentication no',
   match  => '#   PasswordAuthentication yes',
 }
-
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/sshd_config',
