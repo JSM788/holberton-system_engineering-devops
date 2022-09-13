696d896dd381855cc1e36518e49a35f6ac9610f5
JSM788 <salinasjuan788@gmail.com>
2022-09-12 20:07:06 -0500

    Deleting comprobation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,24 +10,24 @@ if __name__ == "__main__":
     response = requests.get(url)
     response2 = requests.get(url2)
 
-    if response.status_code == 200 and response2.status_code == 200:
-        data = response.json()
-        data2 = response2.json()
+    data = response.json()
+    data2 = response2.json()
 
-        number = int(argv[1])
-        txt = "Employee {} is done with tasks".format(data[number - 1]["name"])
-        count = 0
-        count2 = 0
-        lens = len(data2)
-        for x in range(lens):
-            if int(data2[x]["userId"]) == number:
-                count2 += 1
+    number = int(argv[1])
+    txt = "Employee {} is done with tasks".format(data[number - 1]["name"])
+    count = 0
+    count2 = 0
+    lens = len(data2)
 
-        for i in range(lens):
-            if data2[i]["userId"] == number and data2[i]["completed"] is True:
-                count += 1
-        print("{}({}/{}):".format(txt, count, count2))
+    for x in range(lens):
+        if int(data2[x]["userId"]) == number:
+            count2 += 1
 
-        for i in range(lens):
-            if data2[i]["userId"] == number and data2[i]["completed"] is True:
-                print("\t" + data2[i]["title"])
+    for i in range(lens):
+        if data2[i]["userId"] == number and data2[i]["completed"] is True:
+            count += 1
+    print("{}({}/{}):".format(txt, count, count2))
+
+    for i in range(lens):
+        if data2[i]["userId"] == number and data2[i]["completed"] is True:
+            print("\t" + data2[i]["title"])
