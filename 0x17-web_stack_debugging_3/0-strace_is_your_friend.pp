cf453b286bf3e11823a4c57bcbef19d01d0ee305
JSM788 <salinasjuan788@gmail.com>
2022-09-28 23:54:36 -0500

    Fixing route

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixing sevidor with Error
 exec {'Error':
-        command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale-switcher.php/g" /var/www/html/wp-settings.php',
+        command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
         provider => shell,
 }
