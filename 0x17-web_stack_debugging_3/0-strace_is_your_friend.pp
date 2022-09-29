666913c8e72c69da98dd3ba68ea5142d07d6bbd6
JSM788 <salinasjuan788@gmail.com>
2022-09-28 23:51:54 -0500

    Adding comentarie

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Fixing sevidor with Error
 exec {'Error':
         command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale-switcher.php/g" /var/www/html/wp-settings.phpp',
         provider => shell,
